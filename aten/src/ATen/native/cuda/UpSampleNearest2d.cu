65155a551067063467d780739b15330ca3dc103f
Jie <jiej@nvidia.com>
2019-05-16 01:40:23 -0700

    forward seems to work

--- aten/src/ATen/native/cuda/UpSampleNearest2d.cu
+++ aten/src/ATen/native/cuda/UpSampleNearest2d.cu
@@ -11,54 +11,50 @@ namespace at {
 namespace native {
 namespace {
 
+static int lastPow2(unsigned int n) {
+  n |= (n >>  1);
+  n |= (n >>  2);
+  n |= (n >>  4);
+  n |= (n >>  8);
+  n |= (n >> 16);
+  return n - (n >> 1);
+}
+
 template <typename scalar_t, typename accscalar_t>
 #ifdef __HIP_PLATFORM_HCC__
 C10_LAUNCH_BOUNDS_1(1024)
 #endif
 __global__ void upsample_nearest2d_out_frame(
-    const int n,
-    const PackedTensorAccessor<scalar_t, 4> idata,
-    PackedTensorAccessor<scalar_t, 4> odata) {
-  int index = threadIdx.x + blockIdx.x * blockDim.x;
+    const PackedTensorAccessor<scalar_t, 3> idata,
+    PackedTensorAccessor<scalar_t, 3> odata) {
+  int nc_iter = threadIdx.z + blockIdx.z * blockDim.z;
+  int w2 = threadIdx.x + blockIdx.x * blockDim.x;
+  int h2 = threadIdx.y + blockIdx.y * blockDim.y;
 
-  const int batchsize = idata.size(0);
-  const int channels = idata.size(1);
-  const int height1 = idata.size(2);
-  const int width1 = idata.size(3);
-  const int height2 = odata.size(2);
-  const int width2 = odata.size(3);
+  const int nc = idata.size(0);
+  const int height1 = idata.size(1);
+  const int width1 = idata.size(2);
+  const int height2 = odata.size(1);
+  const int width2 = odata.size(2);
+
+  if ( w2 >= width2 || h2 >= height2 ) {
+    return;
+  }
 
   const float height_scale = (float)height1 / (float)height2;
   const float width_scale = (float)width1 / (float)width2;
+  int nc_stride = blockDim.z * gridDim.z;
 
-  if (index < n) {
-    const int w2 = index % width2; // 0:width2-1
-    const int h2 = index / width2; // 0:height2-1
-    // special case: just copy
-    if (height1 == height2 && width1 == width2) {
-      const int h1 = h2;
-      const int w1 = w2;
+  const int h1 = height1 == height2 ? h2 :
+      nearest_neighbor_compute_source_index(height_scale, h2, height1);
+  const int w1 = width1 == width2 ? w2 :
+      nearest_neighbor_compute_source_index(width_scale, w2, width1);
 
-      for (int n = 0; n < batchsize; n++) {
-        for (int c = 0; c < channels; ++c) {
-          const scalar_t val = idata[n][c][h1][w1];
-          odata[n][c][h2][w2] = val;
-        }
-      }
-      return;
-    }
-    //
-    const int h1 =
-        nearest_neighbor_compute_source_index(height_scale, h2, height1);
-    const int w1 =
-        nearest_neighbor_compute_source_index(width_scale, w2, width1);
-
-    for (int n = 0; n < batchsize; n++) {
-      for (int c = 0; c < channels; ++c) {
-        const scalar_t val = idata[n][c][h1][w1];
-        odata[n][c][h2][w2] = val;
-      }
-    }
+  // iterating over 
+  while (nc_iter < nc) {
+    const scalar_t val = idata[nc_iter][h1][w1];
+    odata[nc_iter][h2][w2] = val;
+    nc_iter += nc_stride;
   }
 }
 
@@ -135,6 +131,8 @@ static void upsample_nearest2d_out_cuda_template(
   int input_height = input.size(2);
   int input_width = input.size(3);
 
+  output.resize_({nbatch, channels, output_height, output_width});
+
   upsample_2d_shape_check(
       input,
       Tensor(),
@@ -149,29 +147,45 @@ static void upsample_nearest2d_out_cuda_template(
       input_height > 0 && input_width > 0 && output_height > 0 &&
       output_width > 0);
 
-  output.resize_({input.size(0), input.size(1), output_height, output_width});
-  output.zero_();
+  int nc = input.size(0) * input.size(1);
+  Tensor input_projected = input;
+  Tensor output_projected = output;
+  input_projected.resize_({nc, input_height, input_width});
+  output_projected.resize_({nc, output_height, output_width});
 
-  const int num_kernels = output_height * output_width;
-  const int num_threads =
+  const int max_threads =
       at::cuda::getCurrentDeviceProperties()->maxThreadsPerBlock;
-  cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
+  int block_x = std::min<int>(lastPow2(output_width), max_threads);
+  int block_y = std::min<int>(lastPow2(output_height), max_threads/block_x);
+  int block_z = std::min<int>(nc, max_threads/block_x/block_y);
+
+  int grid_x = cuda::ATenCeilDiv(output_width, block_x);
+  int grid_y = cuda::ATenCeilDiv(output_height, block_y);
+  // maybe we should add a loop here;
+  int grid_z = cuda::ATenCeilDiv(nc, block_z*4);
+
+  const dim3 block(block_x, block_y, block_z);
+  const dim3 grid(grid_x, grid_y, grid_z);
+
+  cudaStream_t stream = at::cuda::getCurrentCUDAStream();
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(
       input.scalar_type(), "upsample_nearest2d_out_frame", [&] {
         using accscalar_t = at::acc_type<scalar_t, true>;
 
-        auto idata = input.packed_accessor<scalar_t, 4>();
-        auto odata = output.packed_accessor<scalar_t, 4>();
+        auto idata = input_projected.packed_accessor<scalar_t, 3>();
+        auto odata = output_projected.packed_accessor<scalar_t, 3>();
 
         upsample_nearest2d_out_frame<scalar_t, accscalar_t>
-            <<<cuda::ATenCeilDiv(num_kernels, num_threads),
-               num_threads,
+            <<<grid,
+               block,
                0,
-               stream>>>(num_kernels, idata, odata);
+               stream>>>(idata, odata);
       });
 
-      AT_CUDA_CHECK(cudaGetLastError());
+  input_projected.resize_({nbatch, channels, input_height, input_width});
+  output_projected.resize_({nbatch, channels, output_height, output_width});
+  AT_CUDA_CHECK(cudaGetLastError());
 }
 
 static void upsample_nearest2d_backward_out_cuda_template(
